74477a2d9afa0b49b6b4d5a66c15ca41ddcc9ce9
Abdullah Azzam <57374597+pemeluksenja@users.noreply.github.com>
2021-05-26 22:54:23 +0700

    Add files via upload

--- xv6-pdx/defs.h
+++ xv6-pdx/defs.h
@@ -9,6 +9,9 @@ struct spinlock;
 struct sleeplock;
 struct stat;
 struct superblock;
+#ifdef CS333_P2
+struct uproc;
+#endif
 
 // bio.c
 void            binit(void);
@@ -124,6 +127,9 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+#ifdef CS333_P2
+int             getprocs(uint max, struct uproc*);
+#endif
 #ifdef CS333_P3
 void            printFreeList(void);
 void            printList(int);
